af6bca5dbfb16a466544458503f31c6445367e85
XxXTomatox <axelmessimv@gmail.com>
2024-02-01 12:57:18 -0600

    segundo_comit
    jejeje

--- index.php
+++ index.php
@@ -5,5 +5,14 @@ $dotenv = Dotenv\Dotenv::createImmutable('./');
 $dotenv->load();
 
 echo $_ENV['mi_variable_de_entorno'];
-
+echo "<br>";
+echo $_ENV['DB_CONNECTION'];
+echo "<br>";
+echo $_ENV['DB_HOST'];
+echo "<br>";
+echo $_ENV['DB_DATABASE'];
+echo "<br>";
+echo $_ENV['DB_USERNAME'];
+echo "<br>";
+echo $_ENV['DB_PASSWORD'];
 ?>
