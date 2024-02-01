5e3891ab46ee72b621d41c1a25affb0e38f8d918
XxXTomatox <axelmessimv@gmail.com>
2024-02-01 14:22:04 -0600

    terser vercion

--- index.php
+++ index.php
@@ -15,4 +15,18 @@ echo "<br>";
 echo $_ENV['DB_USERNAME'];
 echo "<br>";
 echo $_ENV['DB_PASSWORD'];
+
+
+$host = $_ENV['DB_HOST'];
+$database = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
+$conexion = mysqli_connect($host, $username, $password, $database);
+
+if ($conexion) {
+    echo "Conexión exitosa";   
+}else{
+    die("Error de conexión");
+}
 ?>
